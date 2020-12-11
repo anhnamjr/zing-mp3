16496caac2c6187cff70b1057c8b32f613131a16
Nguyen Anh Nam <anhnamnguyen99@gmail.com>
2020-12-11 11:24:46 +0700

    transform data to vuex

---
+++ src/store/modules/album.js
@@ -0,0 +1,248 @@
+const state = {
+  musicHots: [
+    {
+      imgUrl:
+        "https://photo-zmp3.zadn.vn/banner/1/3/e/2/13e23dfe2c5eb64b73b811962ec1e634.jpg",
+    },
+    {
+      imgUrl:
+        "https://photo-zmp3.zadn.vn/banner/b/a/5/3/ba53ac8d31b01866a4bfd98cc24d2705.jpg",
+    },
+    {
+      imgUrl:
+        "https://photo-zmp3.zadn.vn/banner/d/d/3/a/dd3ac08d733a90ee48c6a7a209ba8b53.jpg",
+    },
+    {
+      imgUrl:
+        "https://photo-zmp3.zadn.vn/banner/7/a/8/d/7a8d688a3211153f5c7003a942a84dd8.jpg",
+    },
+    {
+      imgUrl:
+        "https://photo-zmp3.zadn.vn/banner/1/2/e/7/12e7a0ae78122ecda43d817ec1664f0b.jpg",
+    },
+  ],
+  musicLists: [
+    {
+      title: "Click nghe là thích",
+      musics: [
+        {
+          id: 0,
+          imgUrl:
+            "https://photo-resize-zmp3.zadn.vn/w480_r1x1_jpeg/cover/9/3/2/4/93241b5e2dbd479b5c257b93ce2beb3c.jpg",
+          title: "Your XMAS List",
+        },
+        {
+          id: 1,
+          imgUrl:
+            "https://photo-resize-zmp3.zadn.vn/w480_r1x1_jpeg/cover/3/9/7/7/3977856a8f720fe3da97498061367aaa.jpg",
+          title: "Rap Việt Tạo Động Lực",
+        },
+        {
+          id: 2,
+          imgUrl:
+            "https://photo-resize-zmp3.zadn.vn/w480_r1x1_jpeg/cover/d/a/e/9/dae92123e7c55939e2aed04ea1bc189f.jpg",
+          title: "Một Bước Thành Sao",
+        },
+        {
+          id: 3,
+          imgUrl:
+            "https://photo-resize-zmp3.zadn.vn/w480_r1x1_jpeg/cover/1/0/c/4/10c485468a36a0222364d45a8c261027.jpg",
+          title: "Gửi Lời Yêu Thương",
+        },
+        {
+          id: 4,
+          imgUrl:
+            "https://photo-resize-zmp3.zadn.vn/w480_r1x1_jpeg/cover/2/d/6/3/2d63cf92d408126969202b2d7236cdc7.jpg",
+          title: "R&B Việt Ngày Nay",
+        },
+        {
+          id: 5,
+          imgUrl:
+            "https://photo-resize-zmp3.zadn.vn/w480_r1x1_jpeg/cover/e/d/8/f/ed8fd16d327aa4acdf222c25ece3a109.jpg",
+          title: "Nhạc Pop Cho Ngày Giáng Sinh",
+        },
+        {
+          id: 6,
+          imgUrl:
+            "https://photo-resize-zmp3.zadn.vn/w480_r1x1_jpeg/cover/d/b/8/c/db8c5772bf16c2d561d13440ad59bd6c.jpg",
+          title: "K-Pop Today!",
+        },
+        {
+          id: 7,
+          imgUrl:
+            "https://photo-resize-zmp3.zadn.vn/w480_r1x1_jpeg/cover/a/7/a/6/a7a62ac7aac295c457d123468ceaa460.jpg",
+          title: "C-Pop Hot Covers",
+        },
+        {
+          id: 8,
+          imgUrl:
+            "https://photo-resize-zmp3.zadn.vn/w480_r1x1_jpeg/cover/5/a/8/4/5a847d4cb94ff6852a9438e5e6f566d5.jpg",
+          title: "K-Pop Happy Whistle",
+        },
+        {
+          id: 9,
+          imgUrl:
+            "https://photo-resize-zmp3.zadn.vn/w480_r1x1_jpeg/cover/3/a/2/7/3a276c999618c9d492803ea7709f76ea.jpg",
+          title: "OMG: Dance Pop Thời Nay",
+        },
+      ],
+    },
+    {
+      title: "Rộn ràng nhạc mới",
+      musics: [
+        {
+          id: 0,
+          imgUrl:
+            "https://photo-resize-zmp3.zadn.vn/w480_r1x1_jpeg/cover/0/f/7/2/0f72a309c8ff3a96aaeba9e38ef2ebf0.jpg",
+          title: "Nhạc Hàn Hôm Nay Nghe Gì?",
+        },
+        {
+          id: 1,
+          imgUrl:
+            "https://photo-resize-zmp3.zadn.vn/w480_r1x1_jpeg/cover/b/6/1/e/b61e8a4e10a506e1329a34742b8396ff.jpg",
+          title: "Today's Country Hits",
+        },
+        {
+          id: 2,
+          imgUrl:
+            "https://photo-resize-zmp3.zadn.vn/w480_r1x1_jpeg/cover/8/c/f/9/8cf949a6142c633675ba354dd10d3428.jpg",
+          title: "Đỉnh Cao TRENDING",
+        },
+        {
+          id: 3,
+          imgUrl:
+            "https://photo-resize-zmp3.zadn.vn/w480_r1x1_jpeg/cover/6/7/0/0/6700cd729613d697be0ddf185cca9e6d.jpg",
+          title: "V-Pop Nhạc Mới Nổi Bật",
+        },
+        {
+          id: 4,
+          imgUrl:
+            "https://photo-resize-zmp3.zadn.vn/w480_r1x1_jpeg/cover/e/3/1/2/e312a5ccc373ddb2e4a6c69bfb0877ba.jpg",
+          title: "Today's R&B Hits",
+        },
+        {
+          id: 5,
+          imgUrl:
+            "https://photo-resize-zmp3.zadn.vn/w480_r1x1_jpeg/cover/7/c/d/1/7cd125876e0f96427536af0bde2a36c8.jpg",
+          title: "Today's Top Hits",
+        },
+        {
+          id: 6,
+          imgUrl:
+            "https://photo-resize-zmp3.zadn.vn/w480_r1x1_jpeg/cover/d/b/8/c/db8c5772bf16c2d561d13440ad59bd6c.jpg",
+          title: "K-Pop Today!",
+        },
+        {
+          id: 7,
+          imgUrl:
+            "https://photo-resize-zmp3.zadn.vn/w480_r1x1_jpeg/cover/a/7/a/6/a7a62ac7aac295c457d123468ceaa460.jpg",
+          title: "C-Pop Hot Covers",
+        },
+        {
+          id: 8,
+          imgUrl:
+            "https://photo-resize-zmp3.zadn.vn/w480_r1x1_jpeg/cover/5/a/8/4/5a847d4cb94ff6852a9438e5e6f566d5.jpg",
+          title: "K-Pop Happy Whistle",
+        },
+        {
+          id: 9,
+          imgUrl:
+            "https://photo-resize-zmp3.zadn.vn/w480_r1x1_jpeg/cover/3/a/2/7/3a276c999618c9d492803ea7709f76ea.jpg",
+          title: "OMG: Dance Pop Thời Nay",
+        },
+      ],
+    },
+    {
+      title: "Mỉm cười cho ngày vui",
+      musics: [
+        {
+          id: 0,
+          imgUrl:
+            "https://photo-resize-zmp3.zadn.vn/w480_r1x1_jpeg/cover/4/b/e/e/4bee634cb31f13833ec650aa4fabb911.jpg",
+          title: "Nghe Thôi... Đã Thấy Vui!",
+        },
+        {
+          id: 1,
+          imgUrl:
+            "https://photo-resize-zmp3.zadn.vn/w480_r1x1_jpeg/cover/e/3/4/f/e34f647ec174b4b19c4cb1f01231fd8a.jpg",
+          title: "Teen Pop",
+        },
+        {
+          id: 2,
+          imgUrl:
+            "https://photo-resize-zmp3.zadn.vn/w480_r1x1_jpeg/cover/4/4/8/8/44883b3adab9e51e7e36dcf3cb754a00.jpg",
+          title: "K-Pop Cute",
+        },
+        {
+          id: 3,
+          imgUrl:
+            "https://photo-resize-zmp3.zadn.vn/w480_r1x1_jpeg/cover/4/2/6/f/426f70ed262e2bf0a7289216ec636559.jpg",
+          title: "Nhạc Hoa Hot Trên Mạng Xã Hội",
+        },
+        {
+          id: 4,
+          imgUrl:
+            "https://photo-resize-zmp3.zadn.vn/w480_r1x1_jpeg/cover/7/7/3/8/7738daae1b9090b9890336cfc13c371e.jpg",
+          title: "Life Is Good",
+        },
+        {
+          id: 5,
+          imgUrl:
+            "https://photo-resize-zmp3.zadn.vn/w480_r1x1_jpeg/cover/7/c/d/1/7cd125876e0f96427536af0bde2a36c8.jpg",
+          title: "Today's Top Hits",
+        },
+        {
+          id: 6,
+          imgUrl:
+            "https://photo-resize-zmp3.zadn.vn/w480_r1x1_jpeg/cover/d/b/8/c/db8c5772bf16c2d561d13440ad59bd6c.jpg",
+          title: "K-Pop Today!",
+        },
+        {
+          id: 7,
+          imgUrl:
+            "https://photo-resize-zmp3.zadn.vn/w480_r1x1_jpeg/cover/a/7/a/6/a7a62ac7aac295c457d123468ceaa460.jpg",
+          title: "C-Pop Hot Covers",
+        },
+        {
+          id: 8,
+          imgUrl:
+            "https://photo-resize-zmp3.zadn.vn/w480_r1x1_jpeg/cover/5/a/8/4/5a847d4cb94ff6852a9438e5e6f566d5.jpg",
+          title: "K-Pop Happy Whistle",
+        },
+        {
+          id: 9,
+          imgUrl:
+            "https://photo-resize-zmp3.zadn.vn/w480_r1x1_jpeg/cover/3/a/2/7/3a276c999618c9d492803ea7709f76ea.jpg",
+          title: "OMG: Dance Pop Thời Nay",
+        },
+      ],
+    },
+  ],
+  musicChart: [
+    { imgUrl: "//static-zmp3.zadn.vn/skins/zmp3-v5.2/images/song-vn-2x.jpg" },
+    { imgUrl: "//static-zmp3.zadn.vn/skins/zmp3-v5.2/images/mv-vn-2x.jpg" },
+    {
+      imgUrl: "//static-zmp3.zadn.vn/skins/zmp3-v5.2/images/web_song_usuk.png",
+    },
+    {
+      imgUrl: "//static-zmp3.zadn.vn/skins/zmp3-v5.2/images/web_song_kpop.png",
+    },
+  ],
+};
+
+const getters = {
+  allMusicHots: state => state.musicHots,
+  allMusicLists: state => state.musicLists,
+  allMusicChart: state => state.musicChart
+};
+
+const actions = {};
+
+const mutations = {};
+
+export default {
+  state,
+  getters,
+  actions,
+  mutations,
+};
